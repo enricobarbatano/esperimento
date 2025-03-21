0cb49d02891bba2511ff571b31b07f979e93b8bb
enryb <enrybarba2002@gmail.com>
2025-03-21 16:07:13 +0100

    arriva adolfo

--- src/BIBLIOTECA/App.java
+++ src/BIBLIOTECA/App.java
@@ -2,5 +2,7 @@ package BIBLIOTECA;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, !");
+        ElementiBiblioteca e1 = new Libro("mein kampf", "adolfo", "politico", 5000, true);
+        System.out.println(e1.getTitolo());
     }
 }
